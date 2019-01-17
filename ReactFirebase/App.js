555f37818a630df5344782b4f8db1c64286bbd4c
waleed naseem <waleed.naseem1@gmail.com>
2019-01-17 11:52:56 +0500

    Update App.js

--- ReactFirebase/App.js
+++ ReactFirebase/App.js
@@ -15,6 +15,37 @@ firebase.initializeApp(config);
 
 
 export default class App extends Component<Props> {
+
+  constructor(props){
+   super(props)
+   this.state =({
+     email:'',
+     password:'',
+   })
+  }
+  signUp=(email, password)=>{
+   try {
+    if(this.state.password.length<6){
+      alert("please enter atlease 6 characters")
+      return;
+    }
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+     
+   } catch (error) {
+     console.log(error.toString())
+   }
+  }
+
+  loginUser =(email, password)=>{
+    try {
+      firebase.auth().signInWithEmailAndPassword(email, password).then(function(user){
+        console.log(user)
+      })
+    } catch (error) {
+      console.log(error.toString())
+    }
+  }
+
   render() {
     return (
       <Container style={styles.container}>
@@ -26,6 +57,7 @@ export default class App extends Component<Props> {
             <Input 
               autoCorrect={false}
               autoCapitalize="none"
+              onChangeText={(email)=>this.setState({email})}
             />
           </Item>
           <Item floatingLabel>
@@ -36,22 +68,28 @@ export default class App extends Component<Props> {
               secureTextEntry={true}
               autoCorrect={false}
               autoCapitalize="none"
+              onChangeText={(password)=>this.setState({password})}
             />
           </Item>
+
           <Button style={{marginTop:10}}
             full
             rounded
             success
+            onPress={()=> this.loginUser(this.state.email,this.state.password)}
           >
             <Text style={{color:'white'}}>Login</Text>
           </Button>
+
           <Button style={{marginTop:10}}
             full
             rounded
             primary
+            onPress={()=> this.signUp(this.state.email,this.state.password)}
           >
             <Text style={{color:'white'}}>SignUp</Text>
           </Button>
+
         </Form>
       </Container>
     );
